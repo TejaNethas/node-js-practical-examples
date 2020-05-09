483eb30e20d283ff248cd660e01495811d19eafd
Teja_Nethas <saitejau12@gmail.com>
2020-05-09 11:53:43 +0530

    access greet js using require method in node js
    By accessing greetings functions in print the greetings in  print-greetings js

---
+++ print-greetings.js
@@ -0,0 +1,8 @@
+var greetings = require('./greetings');
+
+console.log(`   
+English :  ${greetings.sayHelloInEnglish()} 
+Hindi :  ${greetings.sayHelloInHindi()} 
+Tamil : ${greetings.sayHelloInTamil()} 
+                
+                `);
